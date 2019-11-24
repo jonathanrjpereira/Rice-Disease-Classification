99dfa83a4f717b240f33e143d4900888294b7387
jonathanrjpereira <jonathanrjpereira@gmail.com>
2019-11-24 15:36:54 -0500

    Reshape train/test images

--- classify.py
+++ classify.py
@@ -97,6 +97,10 @@ test_labels = np.array(test_labels)
 
 print(train_images.shape)
 print(test_images.shape)
+
+train_images = train_images.reshape(train_images.shape[0], 200, 200, 1)
+test_images = test_images.reshape(test_images.shape[0], 200, 200, 1)
+
 # print(train_labels)
 # print(test_labels)
 
@@ -119,17 +123,37 @@ test_images = test_images / 255.0
 #     plt.xlabel(class_names[train_labels[i]])
 # plt.show()
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(200, 200)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(4, activation='softmax')
-])
+# model = keras.Sequential([
+#     keras.layers.Flatten(input_shape=(200, 200)),
+#     keras.layers.Dense(128, activation='relu'),
+#     keras.layers.Dense(4, activation='softmax')
+# ])
+#
+# model.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+#
+# model.fit(train_images, train_labels, epochs=10)
+#
+# test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+#
+# print('\nTest accuracy:', test_acc)
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(200, 200, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(4, activation='softmax'))
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=3)
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
